3db348dc26c9a42b9762ea382c74b48fdbd10ac7
Coding-Ocean <eijo524@gmail.com>
2023-12-11 18:01:59 +0900

    cout -> printf

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
-struct FLOAT
+class FLOAT
 {
+public:
 	union
 	{
 		float f;
@@ -40,7 +41,42 @@ struct FLOAT
 			unsigned char b31 : 1;
 		};
 	};
-	FLOAT(float f):f(f){}
+	unsigned char bits[32];
+
+	FLOAT(float f):f(f){
+		bits[0] = b31;
+		bits[1] = b30;
+		bits[2] = b29;
+		bits[3] = b28;
+		bits[4] = b27;
+		bits[5] = b26;
+		bits[6] = b25;
+		bits[7] = b24;
+		bits[8] = b23;
+		bits[9] = b22;
+		bits[10] = b21;
+		bits[11] = b20;
+		bits[12] = b19;
+		bits[13] = b18;
+		bits[14] = b17;
+		bits[15] = b16;
+		bits[16] = b15;
+		bits[17] = b14;
+		bits[18] = b13;
+		bits[19] = b12;
+		bits[20] = b11;
+		bits[21] = b10;
+		bits[22] = b09;
+		bits[23] = b08;
+		bits[24] = b07;
+		bits[25] = b06;
+		bits[26] = b05;
+		bits[27] = b04;
+		bits[28] = b03;
+		bits[29] = b02;
+		bits[30] = b01;
+		bits[31] = b00;
+	}
 	void dispFloat()
 	{
 		printf("%f\n", f);
@@ -48,12 +84,6 @@ struct FLOAT
 	void dispBin()
 	{
 		//２進数表示
-		unsigned char bits[] = { b31,b30,
-			b29,b28,b27,b26,b25,b24,b23,b22,b21,b20,
-			b19,b18,b17,b16,b15,b14,b13,b12,b11,b10,
-			b09,b08,b07,b06,b05,b04,b03,b02,b01,b00,
-		};
-		printf("２進に変換\n");
 		for (int i = 0; i < 32; i++) {
 			printf("%u", bits[i]);
 			if (i == 0 || i == 8)printf(" ");
@@ -62,12 +92,6 @@ struct FLOAT
 	}
 	void dispDec()
 	{
-		//２進数表示
-		unsigned char bits[] = { b31,b30,
-			b29,b28,b27,b26,b25,b24,b23,b22,b21,b20,
-			b19,b18,b17,b16,b15,b14,b13,b12,b11,b10,
-			b09,b08,b07,b06,b05,b04,b03,b02,b01,b00,
-		};
 		printf("10進に変換\n");
 		//符号
 		int s = bits[0];
@@ -86,71 +110,94 @@ struct FLOAT
 			pow(-1, s), //符号
 			pow(2, e - 127), //指数部
 			1 + m / pow(2, 23), //仮数部
-			pow(-1, s) * pow(2, e - 127) * (1 + m / pow(2, 23)));
-
+			pow(-1, s) * pow(2, e - 127) * (1 + m / pow(2, 23))
+		);
 	}
 };
+
 void toBin(float f)
 {
+	//引数ｆの整数部が-32768～32767であること！
+	if (int(f) < -32768 || int(f) > 32767) {
+		printf("範囲外の引数\n");
+		return;
+	}
+
 	printf("%f\n",f);
 
+	//符号
 	int sign = f < 0 ? 1 : 0;
-	cout << (sign ? "minus" : "plus") << endl;
+	printf("%s\n", (sign ? "minus" : "plus"));
 
-	//整数部２進数文字列化
-	f = fabsf(f);
-	int Int = int(f);
-	f -= Int;//小数点以下を取り出す。後で使う
-	char c1[16 + 32 + 1]{};
-	for (int i = 15; i >= 0; i--) {
-		c1[i] = Int % 2 + '0';
+	//配列ｃに、整数部と小数部を分けて、2進数化した数を並べていきます。
+	const int n1 = 16;//整数部の桁数
+	const int n2 = 64;//小数部の桁数
+	char c[n1 + n2]{};
+	//整数部２進数配列化
+	f = fabsf(f);//絶対値化して
+	int Int = int(f);//整数部を取り出す
+	for (int i = n1-1; i >= 0; i--) {
+		c[i] = Int % 2;
 		Int /= 2;
 	}
-	//小数部２進数文字列化
-	for (int i = 16; i < 16 + 31; i++) {
+	//小数部２進数配列化
+	f -= int(f);//小数点以下を取り出す
+	for (int i = n1; i < (n1 + n2); i++) {
 		f *= 2;
-		c1[i] = int(f) + '0';
-		if (int(f) == 1)f -= 1.0f;
+		c[i] = int(f);
+		if (int(f) == 1) {
+			f -= 1.0f;
+		}
 	}
-	//何ビットシフトするか求める
-	bool flag = true;
-	int e = 0;
-	int disp = 0;
-	for (int i = 0; i < 16 + 32; i++) {
-		if (flag && c1[i] == '1') { e = 15 - i; disp = i + 1; flag = false; }
-	}
-	cout << e << "ビットシフトする\n";
+
 	//整数部表示
-	for (int i = 0; i < 16; i++) {
-		cout << c1[i];
+	for (int i = 0; i < n1; i++) {
+		printf("%d",c[i]);
 	}
 	//小数部表示
-	cout << '.';
-	for (int i = 16; i < 16 + 32; i++) {
-		cout << c1[i];
+	printf(".");
+	for (int i = n1; i < (n1 + n2); i++) {
+		printf("%d", c[i]);
 	}
-	cout << endl;
+	printf("\n");
 
-	e += 127;
-	char e_[8 + 1]{};
+	//何桁浮動するか求める
+	int shift = 0;
+	int disp = 0;
+	for (int i = 0; i < n1 + n2; i++) {
+		if (c[i] == 1) {
+			shift = n1 - 1 - i;
+			disp = i + 1;
+			break;
+		}
+	}
+	cout << shift << "桁浮動する\n";
+	//指数部の２進配列化
+	shift += 127;
+	char e[8 + 1]{};
 	for (int i = 7; i >= 0; i--) {
-		e_[i] = e % 2 + '0';
-		e /= 2;
+		e[i] = shift % 2;
+		shift /= 2;
 	}
 
 	//floatビット表示
-	cout << sign << ' ';
-	cout << e_ << ' ';
-	for (int i = disp; i < 23 + disp; i++) {
-		cout << c1[i];
+	printf("%d ", sign);
+	for (int i = 0; i < 8; i++) {
+		printf("%d", e[i]);
 	}
-	cout << endl;
+	printf(" ");
+	for (int i = disp; i < disp+23; i++) {
+		printf("%d", c[i]);
+	}
+	printf("\n");
 }
+
 int main()
 {
-	struct FLOAT f { .2f };
+	FLOAT f { 0.125f };
 	f.dispFloat();
 	f.dispBin();
+	//f.dispDec();
 
 	cout << endl;
 	toBin(f.f);
